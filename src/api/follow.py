814ab07aa701cd9f9c4bd58fda2c1d575e0bb0b9
Whoiskp <khoapham.ptithcm@gmail.com>
2021-10-08 08:41:06 +0700

    add follow infor of author

--- src/api/follow.py
+++ src/api/follow.py
@@ -12,6 +12,7 @@ import src.schemas.event as SchemaEvent
 import src.schemas.track as SchemaTrack
 import src.schemas.tweet as SchemaTweet
 import src.schemas.album as SchemaAlbum
+import src.schemas.user as SchemaUser
 import src.decorators as Decorators
 
 bp = Blueprint('follow', __name__, url_prefix='/api/follow')
@@ -32,6 +33,7 @@ def item_action(user_info, rtype, oid):
         collection = Repo.mTweet
     if rtype == Consts.RESOURCE_TYPE_AUTHOR:
         collection = Repo.mUser
+        oid = int(oid)
     item = collection.get_item(oid)
     if not item:
         return {
@@ -94,23 +96,11 @@ def item_action(user_info, rtype, oid):
 @Http.make_cross_resp
 @Decorators.require_login
 def item_follow_list(user_info, rtype):
-    collection = Repo.mTrack
-    schema = SchemaTrack
-    if rtype == Consts.RESOURCE_TYPE_EVENT:
-        collection = Repo.mEvent
-        schema = SchemaEvent
-    if rtype == Consts.RESOURCE_TYPE_ALBUM:
-        collection = Repo.mAlbum
-        schema = SchemaAlbum
-    if rtype == Consts.RESOURCE_TYPE_TWEET:
-        collection = Repo.mTweet
-        schema = SchemaTweet
-
     page = py_.get(request.args, 'page', 1)
     page = py_.to_integer(page) or 1
     page_size = Consts.PAGE_SIZE_DEFAULT
     uid = py_.get(user_info, 'id', -1)
-    objs = RepoResource.aggregate([
+    pipelines = [
         {"$match": {"type": rtype, "followers": uid}},
         {"$addFields": {"_oid": {"$toObjectId": "$oid"}}},
         {"$project": {"followers": 0}},
@@ -127,13 +117,46 @@ def item_follow_list(user_info, rtype):
         {"$sort": {"data.title": 1}},
         {'$skip': int((page - 1) * page_size)},
         {'$limit': page_size},
-    ])
+    ]
+    collection = Repo.mTrack
+    schema = SchemaTrack.Item
+    if rtype == Consts.RESOURCE_TYPE_EVENT:
+        collection = Repo.mEvent
+        schema = SchemaEvent.Item
+    if rtype == Consts.RESOURCE_TYPE_ALBUM:
+        collection = Repo.mAlbum
+        schema = SchemaAlbum.Item
+    if rtype == Consts.RESOURCE_TYPE_TWEET:
+        collection = Repo.mTweet
+        schema = SchemaTweet.Item
+    if rtype == Consts.RESOURCE_TYPE_AUTHOR:
+        collection = Repo.mUser
+        schema = SchemaUser.PublicItem
+        pipelines = [
+            {"$match": {"type": rtype, "followers": uid}},
+            {"$project": {"followers": 0}},
+            {
+                '$lookup': {
+                    'from': 'user',
+                    'localField': "oid",
+                    'foreignField': "_id",
+                    'as': "data"
+                }
+            },
+            {"$unwind": "$data"},
+            {"$match": {"data.status": {"$ne": "inactive"}}},
+            {"$sort": {"data.title": 1}},
+            {'$skip': int((page - 1) * page_size)},
+            {'$limit': page_size},
+        ]
+
+    objs = RepoResource.aggregate(pipelines)
     data = [py_.get(obj, 'data') for obj in objs]
     data = py_.map_(data, Repo.mUser.map_item_user_info)
 
     return {
         "status": Consts.STATUS_OK,
         "error_code": HTTPStatus.OK,
-        "data": schema.Item(many=True).dump(data),
+        "data": schema(many=True).dump(data),
         "msg": "success"
     }

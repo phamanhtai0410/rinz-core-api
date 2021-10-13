f60c8592a223de13a75bef91869e10b272da8d5f
Whoiskp <khoapham.ptithcm@gmail.com>
2021-10-13 10:13:11 +0700

    fix bug cmt

--- src/api/event.py
+++ src/api/event.py
@@ -67,7 +67,7 @@ def get_item(user_info, oid):
                 # Publish Message to Socket Channel
                 payload_pub = {
                     "type": "control",
-                    "content": "enable_comment" if item["enable_comment"] else "disable_comment",
+                    "content": "enable_comment" if i_cmt else "disable_comment",
                     # "user": {
                     #     "user_name": py_.get(user_info, 'user_full_name', ''),
                     #     "user_avatar": py_.get(user_info, 'user_avatar', ''),
